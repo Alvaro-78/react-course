0266c4921b1923291906ee4fa679033ea16e4538
Alvaro-78 <alvaro78an@gmail.com>
2023-01-18 18:40:40 +0100

    Added Jest test

---
+++ counter-vite/test/demo.test.js
@@ -0,0 +1,12 @@
+describe('Pruebas en Demo', () => {
+	test('Test de Prueba', () => {
+		// 1. Inicialización
+		const message1 = 'Hola Pepe';
+
+		// 2. Estímulo
+		const message2 = 'Hola Pepe';
+
+		// 3. Observar el comportamiento
+		expect(message1).toBe(message2);
+	});
+});
