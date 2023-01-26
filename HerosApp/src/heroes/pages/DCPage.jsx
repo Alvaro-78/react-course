3fb31a0d5ed8cc6a89c0ebc4ba8f96e0b9578f4d
Alvaro-78 <alvaro78an@gmail.com>
2023-01-26 13:31:11 +0100

    Added HeroList component with publisher

--- HerosApp/src/heroes/pages/DCPage.jsx
+++ HerosApp/src/heroes/pages/DCPage.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import { HeroList } from '../components';
 
 export const DCPage = () => {
 	return (
 		<>
 			<h1>DC Page</h1>
+			<hr />
+			<HeroList publisher={'DC Comics'} />
 		</>
 	);
 };
-
--- HerosApp/src/heroes/pages/HeroPage.jsx
+++ HerosApp/src/heroes/pages/HeroPage.jsx
@@ -1,6 +1,20 @@
-import React from 'react';
+import { Navigate, useParams } from 'react-router-dom';
+import { getHeroById } from '../getHeroById';
 
 export const HeroPage = () => {
+	// Dentro de useParams sacamos el id
+	const { id } = useParams();
+
+	// Usamos la función getHerById para obtener a trabes del id los datos
+	const hero = getHeroById(id);
+
+	// Con esta condicion redirigimos al usuario si la url no tiene héroes o se intenta poner una url falsa
+	if (!hero) {
+		return <Navigate to="/" />;
+	}
+
+	console.log(hero);
+
 	return (
 		<>
 			<h1>HeroPage</h1>
